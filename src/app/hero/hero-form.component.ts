6b19365e1bc0d0ef367abee2c6ed9846d64a3382
lijo2 <jonathan_li@wsib.on.ca>
2020-01-27 14:48:15 -0500

    update

--- src/app/hero/hero-form.component.ts
+++ src/app/hero/hero-form.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-hero-form',
   templateUrl: './hero-form.component.html',
   styleUrls: ['./hero-form.component.css']
 })
-export class HeroFormComponent {
+export class HeroFormComponent implements OnInit{
 
   powers = ['Really Smart', 'Super Flexible',
             'Super Hot', 'Weather Changer'];
 
   options = ['sun', 'moon', 'earth', 'plane', '123', '234'];
+  filteredOptions: Observable<string[]>;
 
   myControl = new FormControl('');
 
@@ -29,5 +32,18 @@ export class HeroFormComponent {
   newHero() {
     this.model = new Hero(42, '', '');
     console.log('new hero', this.diagnostic);
+  }
+
+  ngOnInit() {
+      this.filteredOptions = this.myControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filter(value))
+      );
+  }
+
+  private _filter(value: string): string[] {
+      const filterValue = value.toLowerCase();
+      return this.options.filter(option =>
+        option.toLowerCase().includes(filterValue));
   }
 }
